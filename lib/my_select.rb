daba8d8597c82f6a34a422a2ef98a92e2d3999b9
Linzell Godfrey <linzell.godfrey@live.co.uk>
2018-12-05 15:38:26 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,14 @@
 def my_select(collection)
- # your code here!
+  if block_given?
+    new_array = []
+    i = 0 
+    while i < collection.length
+    yield collection[i]
+    i += 1 
+    end
+    new_array
+  else
+    return "This block should not run!"
+  end
 end
+
